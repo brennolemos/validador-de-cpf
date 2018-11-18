62b70fa28ef6d3aca482e4fade6af731a6d42b1e
brennolemos <brenomiros@gmail.com>
2018-11-18 14:21:25 -0300

    Validador de CPF Funcionando

--- Validar CPF/validar-cpf.js
+++ Validar CPF/validar-cpf.js
@@ -5,11 +5,43 @@ export default class ValidarCPF {
   limpar(cpf) {
     return cpf.replace(/\D/g, '');
   }
-  construir (cpf) {
+  construir(cpf) {
     return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, '$1.$2.$3-$4');
   }
   formatar(cpf) {
     const cpfLimpo = this.limpar(cpf);
     return this.construir(cpfLimpo);
+  }
+  validar(cpf) {
+    const matchCPF = cpf.match(/(?:\d{3}[-.\s]?){3}\d{2}/g);
+    return (matchCPF && matchCPF[0] === cpf);
+  }
+  validarNaMudanca(cpfElement) {
+    if (this.validar(cpfElement.value)) {
+      cpfElement.value = this.formatar(cpfElement);
+      cpfElement.classList.add('valido');
+      cpfElement.classList.remove('erro');
+      cpfElement.nextElementSibling.classList.remove('ativar');      
+    } else {
+      cpfElement.classList.add('erro');
+      cpfElement.classList.remove('valido');
+      cpfElement.nextElementSibling.classList.add('ativar');
+    }
+  }
+  adicionarEvento() {
+    this.element.addEventListener('change', () => {
+      this.validarNaMudanca(this.element);
+    })
+  }
+  adicionarErroSpan() {
+    const erroElement = document.createElement('span');
+    erroElement.classList.add('erro-text');
+    erroElement.innerText = 'CPF Inválido';
+    this.element.parentElement.insertBefore(erroElement, this.element.nextElementSibling);
+  }
+  iniciar() {
+    this.adicionarEvento();
+    this.adicionarErroSpan();
+    return this;
   }
 }
